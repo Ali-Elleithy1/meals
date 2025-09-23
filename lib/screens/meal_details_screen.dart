a8f3c1af70a15851f50134dbf331b38dba7b166f
Ali-Elleithy1 <ali.kh1615@gmail.com>
2025-09-23 08:45:14 +0300

    Create meal details screen

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -3,18 +3,80 @@ import 'package:meals/models/meal.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealDetailsScreen extends StatelessWidget {
-  const MealDetailsScreen({super.key, required this.meal});
+  const MealDetailsScreen({
+    super.key,
+    required this.meal,
+    required this.onToggleFavorites,
+  });
 
   final Meal meal;
+  final void Function(Meal meal) onToggleFavorites;
 
   @override
   Widget build(context) {
     return Scaffold(
-      appBar: AppBar(title: Text(meal.title)),
-      body: Container(
-        child: FadeInImage(
-          placeholder: MemoryImage(kTransparentImage),
-          image: NetworkImage(meal.imageUrl),
+      appBar: AppBar(
+        title: Text(meal.title),
+        actions: [
+          IconButton(
+            onPressed: () {
+              onToggleFavorites(meal);
+            },
+            icon: Icon(Icons.star),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            FadeInImage(
+              width: double.infinity,
+              height: 300,
+              fit: BoxFit.cover,
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(meal.imageUrl),
+            ),
+            SizedBox(height: 14),
+            Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.primary,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 14),
+            for (var ingredient in meal.ingredients)
+              Text(
+                ingredient,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  color: Theme.of(context).colorScheme.onSurface,
+                ),
+              ),
+            SizedBox(height: 14),
+            Text(
+              'Steps',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.primary,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 14),
+            ...meal.steps.map(
+              (step) => Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 8,
+                ),
+                child: Text(
+                  step,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurface,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
